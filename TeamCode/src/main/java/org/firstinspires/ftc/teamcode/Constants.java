4a8df43df73572400d55369ef728af156a8cf4b2
Jadon Xu <jadonjxu@gmail.com>
2026-03-10 16:06:47 -0400

    commit constants

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+public class Constants {
+    public static String HMimu = "imu";
+    public static String HMSpindexerEncoder = "spinDexerEncoder";
+    public static String HMTurretEncoder = "turretEncoder";
+    public static String HMPinPointer = "pinpointer";
+
+    public static String HMMotorFrontLeft = "frontLeft";
+    public static String HMMotorFrontRight = "frontRight";
+    public static String HMMotorBackLeft = "backLeft";
+    public static String HMMotorBackRight = "backRight";
+
+    public static String HMLimelight = "limelight";
+
+    public static String HMMotorShooter1 = "shooter1";
+    public static String HMMotorShooter2 = "shooter2";
+    public static String HMMotorIntake = "intake";
+    public static String HMMotorPopper = "popper";
+
+    public static String HMFrontColorSensor = "colorSensorFront";
+    public static String HMBackColorSensor = "colorSensorBack";
+    public static String HMServoPopper = "popperServo";
+    public static String HMServoTurretLeft = "turretLeft";
+    public static String HMServoTurretRight = "turretRight";
+    public static String HMServobackSpin = "backSpinServo";
+    public static String HMServospinDexer = "spinDexerServo";
+    public static String HMServoLight = "Light";
+}
